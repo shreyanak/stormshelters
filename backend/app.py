6b792f2feff94ca11bb3b8a7b1652efc4f16b62f
Shreya Nakka <shreyanakka@wireless-10-146-31-111.public.utexas.edu>
2023-10-11 17:45:37 -0500

    model.py

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from models import app
 
-app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return ""
 
-if __name__ == '__main__':
-    app.run()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
