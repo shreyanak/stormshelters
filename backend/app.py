42cf453ad98b8084c6d9cddf85412b486ca26c8e
aaron-kulkarni <aaron.d.kulkarni@gmail.com>
2023-10-11 16:02:26 -0500

    initial endpoints and schemas

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, Response, session, request, jsonify
+from models import db, app, Cities, Pharmacy, Shelter
+from schema import cities_schema, pharmacy_schema, shelter_schema
+
 
 app = Flask(__name__)
 
@@ -6,5 +9,68 @@ app = Flask(__name__)
 def home():
     return ""
 
+
+@app.route('/cities',  methods=['GET'])
+def get_cities():
+    query = db.session.query(Cities)
+    result = query.paginate(page=1, per_page=10, error_out=False) 
+
+    schema_dump = cities_schema.dump(result, many=True)
+    for city in schema_dump:
+        split_strings_city(city)   
+    
+    total = result.count()     
+    return jsonify({"cities" : schema_dump,
+                    "meta": {
+                        "count": total
+                    }})
+
+@app.route('/pharmacies',  methods=['GET'])
+def get_cities():
+    query = db.session.query(Pharmacy)
+    result = query.paginate(page=1, per_page=30, error_out=False) 
+
+    schema_dump = pharmacy_schema.dump(result, many=True)
+    for pharmacy in schema_dump:
+        split_strings_pharmacy(pharmacy)   
+    
+    total = result.count()     
+    return jsonify({"pharmacies" : schema_dump,
+                    "meta": {
+                        "count": total
+                    }})
+
+
+@app.route('/shelters',  methods=['GET'])
+def get_cities():
+    query = db.session.query(Shelter)
+    result = query.paginate(page=1, per_page=10, error_out=False) 
+
+    schema_dump = shelter_schema.dump(result, many=True)
+    for shelter in schema_dump:
+        split_strings_shelter(shelter)   
+    
+    total = result.count()     
+    return jsonify({"shelters" : schema_dump,
+                    "meta": {
+                        "count": total
+                    }})
+                    
+
+
+
+
+def split_strings_city(city):
+    city['shelters'] = list(filter(None,city['shelters'].split(';')))
+    city['pharmacies'] = list(filter(None,city['pharmacies'].split(';')))
+def split_strings_pharmacy(pharmacy):
+    pharmacy['shelters'] = list(filter(None,pharmacy['shelters'].split(';')))
+    pharmacy['cities'] = list(filter(None,pharmacy['cities'].split(';')))
+def split_strings_shelter(shelter):
+    shelter['pharmacies'] = list(filter(None,shelter['pharmacies'].split(';')))
+    shelter['cities'] = list(filter(None,shelter['cities'].split(';')))
+
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
