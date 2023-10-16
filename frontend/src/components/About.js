513aea9c1c3616444524b061624a3b9e90676e94
Shreya Nakka <shreyanakka@Shreyas-MacBook-Air-4.local>
2023-10-16 12:24:55 -0500

    some changes

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -10,7 +10,7 @@ function About() {
     const projectId = '50438097';
     const contributorData = [
       { name: 'aaronk2711', displayName: 'Aaron Kulkarni', name3: 'aaron-kulkarni', description: 'I am a CS major currently in my third year from New Jersey. My favorite activities outside of the classroom are basketball, judo, and guitar. Major Responsibilities: Backend'},
-      { name: 'shreya.n', displayName: 'Shreya Nakka', name3: '', description: 'I am a Computer Science major in my Junior year. In my free time, I enjoy playing tennis, volleyball, and painting. Major Responsibilities: Frontend'},
+      { name: 'shreya.n', displayName: 'Shreya Nakka', name3: '', description: 'I am a Computer Science major in my Junior year. In my free time, I enjoy playing tennis, volleyball, and painting. Major Responsibilities: Backend'},
       { name: 'rohitc28', displayName: 'Rohit Chawla', name3: '', description: 'I am a Computer Science major in my Junior year. I have lived in Austin my entire life and I enjoy playing board games. Major Responsibilities: Frontend'},
       { name: 'jrsmith0', displayName: 'John Smith', name3: '', description: 'I am a junior computer science major from the Rio Grande Valley. In my free time, I enjoy camping and spending time with family. Major Responsibilities: Frontend'},
       { name: 'areye2020', displayName: 'Adriana Reyes', name3: '', description: 'I am a senior computer science major from Austin. Outside of CS I like to play guitar, skateboard, and draw. Major responsibilities: Frontend'},
