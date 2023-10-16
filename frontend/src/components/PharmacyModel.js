9a29151d71060e9841672ac203c04f48c16e5725
jrsmith0 <johnrsmith20003@gmail.comqsfuckgit config --global user.email johnrsmith20003@gmail.com>
2023-10-16 16:04:10 -0500

    more backend.js and frontend fetch workaround

--- frontend/src/components/PharmacyModel.js
+++ frontend/src/components/PharmacyModel.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../css/City.css';
+// import axios from 'axios';
 
-const CityCard = ({ cityData }) => {
-  function connectDB() {
-    // Database connection, since Pharmiacies pulls from multiple JSON files
-    const mysql = require('mysql');
-    const con = mysql.createConnection({
-      host:  "stormshelters-db.clwbujmk0ylk.us-east-2.rds.amazonaws.com",
-      user: "admin",
-      password: "StormShelters2023",
-      database: "models"
-    });
-    con.connect();
-  }
-  connectDB();
-  const pharmacyData = con.query("SELECT * from pharmacies_new");
-  console.log(pharmacyData);
+// todo: we need to replace the URL with the production URL for our backend server
+
+
+const PharmacyCard = ({ cityData }) => {
+  const { name, pop, temp, wind_mph, condition, precip_in } = cityData;
 
   return (
     <div className="card">
@@ -39,4 +30,4 @@ const CityCard = ({ cityData }) => {
   );
 };
 
-export default CityCard;
+export default PharmacyCard;
