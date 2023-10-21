c808579f33f2d3845da6d1ca8dadec13c2192071
Adriana Reyes <areye2020@gmail.com>
2023-10-20 23:28:21 -0500

    add model cards to home page and fix varaible names

--- frontend/src/components/CityDetail.js
+++ frontend/src/components/CityDetail.js
@@ -21,7 +21,7 @@ function CityDetail() {
   console.log(cityData);
 
 
-  const { city_name, pop, 'temp in f': temp, wind_mph, condition, precip_in } = cityData;
+  const { city_name, pop, temp_in_f, wind_mph, cond, precip_in } = cityData;
 
   return (
     <div className="city-detail-container">
@@ -38,13 +38,13 @@ function CityDetail() {
           <p>Population: {pop}</p>
         </div>
         <div className="city-detail-card">
-          <p>Temperature: {temp}</p>
+          <p>Temperature: {temp_in_f}</p>
         </div>
         <div className="city-detail-card">
           <p>Wind: {wind_mph}</p>
         </div>
         <div className="city-detail-card">
-          <p>Conditions: {condition}</p>
+          <p>Conditions: {cond}</p>
         </div>
         <div className="city-detail-card">
           <p>Precipitations: {precip_in}</p>
