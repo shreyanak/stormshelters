430eef017fa8f1a4ee8edff3b1684dbf22f4abcc
aaron-kulkarni <aaron.d.kulkarni@gmail.com>
2023-11-04 19:43:08 -0500

    jest tests done

--- frontend/src/__tests__/jest.test.js
+++ frontend/src/__tests__/jest.test.js
@@ -1,5 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
+import { render, screen, fireEvent, cleanup, waitFor } from '@testing-library/react';
 
 
 import App from '../App.js'
@@ -8,7 +9,15 @@ import Navbar from '../components/Navbar.js';
 
 import About from '../components/About.js';
 import Home from '../components/Home.js';
-import CityCard from '../components/CityModel.js';
+import Cities from '../components/Cities.js';
+import Pharmacies from '../components/Pharmacies.js';
+import Shelters from '../components/Shelter.js';
+// import CityCard from '../components/CityModel.js';
+
+afterEach(() => {
+  cleanup();
+});
+
 
 it('Init NavBar', () => {
     const component = renderer.create(
@@ -23,15 +32,12 @@ it('Init NavBar', () => {
   
 
   it('Init About', () => {
-    const component = renderer.create(
-      <BrowserRouter>
-        <About />
-      </BrowserRouter>,
-    );
+    const component = renderer.create(<BrowserRouter><About /></BrowserRouter>);
   
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
+
 
   it('Init App', () => {
     const component = renderer.create(<App />);
@@ -51,16 +57,50 @@ it('Init NavBar', () => {
     expect(tree).toMatchSnapshot();
   });
 
-//   it('Init CityCard', () => {
-//     const component = renderer.create(
-//       <BrowserRouter>
-//         <CityCard />
-//       </BrowserRouter>,
-//     );
+
+  test('Home renders', async () => {
+    render(<BrowserRouter><Home /></BrowserRouter>);
+    await waitFor(() => {
+      expect(screen.getByText(/Prepare for the Storms./)).toBeInTheDocument();
+    });
+  });
   
-//     let tree = component.toJSON();
-//     expect(tree).toMatchSnapshot();
-//   });
+  test('About renders', async () => {
+    render(<BrowserRouter><About /></BrowserRouter>);
+    await waitFor(() => {
+      expect(screen.getByText(/About Page/)).toBeInTheDocument();
+    });
+  });
+
+
+  test('Leagues renders', async () => {
+    render(<BrowserRouter><Cities /></BrowserRouter>);
+    await waitFor(() => {
+      expect(screen.getByText('Cities')).toBeInTheDocument();
+    });
+  });
   
+
+  test('Shelters renders', async () => {
+    render(<BrowserRouter><Shelters /></BrowserRouter>);
+    await waitFor(() => {
+      expect(screen.getByText('Shelters & Food pantries')).toBeInTheDocument();
+    });
+  });
+  
+
+  test('Pharmacies renders', async () => {
+    render(<BrowserRouter><Pharmacies /></BrowserRouter>);
+    await waitFor(() => {
+      expect(screen.getByText('Pharmacies')).toBeInTheDocument();
+    });
+  });
+
+  test('About has tools', async () => {
+    render(<BrowserRouter><About /></BrowserRouter>);
+    await waitFor(() => {
+      expect(screen.getByText('Tools Used:')).toBeInTheDocument();
+    });
+  });
   
   
