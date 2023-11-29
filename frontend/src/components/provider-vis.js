55fc4c2cc9767b81e88b0866894cbb4dbec3101a
jrsmith0 <johnrsmith20003@gmail.comqsfuckgit config --global user.email johnrsmith20003@gmail.com>
2023-11-29 11:03:33 -0600

    skeleton

--- frontend/src/components/provider-vis.js
+++ frontend/src/components/provider-vis.js
@@ -10,6 +10,11 @@ import * as d3 from "d3";
 
 const metroAPI = 'https://backend.metropulse.link/';
 
+// https://backend.metropulse.link/centers?borough=bronx
+
+function getData() {
+    
+}
 
 function ProviderVis() {
     return (
