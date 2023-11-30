8670547da77f7f1f6fd59a21038fc8c75d05155d
aaron-kulkarni <aaron.d.kulkarni@gmail.com>
2023-11-29 21:58:09 -0600

    provider visualization added

--- frontend/src/components/provider-vis.js
+++ frontend/src/components/provider-vis.js
@@ -11,7 +11,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import '../css/Visualization.css';
 
-import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid, Legend, Line, LineChart } from 'recharts';
+import { PieChart, Pie, Cell, BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid, Legend, Line, LineChart, Label } from 'recharts';
 
 // https://backend.metropulse.link/centers?borough=bronx
 
@@ -30,6 +30,25 @@ function getCenters() {
         var data = { name: boroughs[borough], Centers: number };
         values[borough] = data;
     }
+    return values;
+}
+
+function getFacilities() {
+    var boroughs = new Array('Bronx', 'Brooklyn', 'Manhattan', 'Queens');
+    var values = [];
+    for (var borough in boroughs) {
+        var mainURL = `https://backend.metropulse.link/hospitals?borough=${boroughs[borough]}`;
+        console.log(mainURL);
+        var mainreq = new XMLHttpRequest();
+        mainreq.open('GET', mainURL, false); 
+        mainreq.send(null);
+        // We get just the size from API
+        var number = JSON.parse(mainreq.responseText).total_size;
+        // Prepare the data in a fortmat that Recharts will like
+        var data = { name: boroughs[borough], Centers: number };
+        values[borough] = data;
+    }
+    console.log(values);
     return values;
 }
 
@@ -85,10 +104,12 @@ function ProviderVis() {
     var popRanges = getPops();
     console.log(popRanges[1].Number)
     var centersPerBorough = getCenters();
+    var facilitiesPerBorough = getFacilities();
+    const COLORS = ["#0088FE", "#00C49F", "#483d8b", "#d8bfd8", "#6495ed", "#00bfff", "#add8e6", "#b0c4de", "#b0e0e6"];
     // console.log("mydata: " + myvals);
 
     return (
-        // Display for Number of TEsting Centers Per Borough
+        // Display for Number of Testing Centers Per Borough
         <Container>
             <h1 className="text-center">MetroPulse Visualizations</h1>
 
@@ -117,6 +138,34 @@ function ProviderVis() {
                     <Line type="monotone" dataKey="Number" fill="#515487" width="20%"/>
                 </LineChart>
             </ResponsiveContainer>
+
+            <h2 className="text-center">Medical Facililtes by Borough</h2>
+
+            <ResponsiveContainer width = "100%" height={450}>
+                <PieChart width = {800} height={600}>
+                    <Pie
+                        dataKey="Centers"
+                        nameKey="name"
+                        data={facilitiesPerBorough}
+                        cx="50%"
+                        cy="50%"
+                        outerRadius={150}
+                        fill="#8884d8"
+                    >
+                    {facilitiesPerBorough.map((entry, index) => (
+                                    <Cell
+                                        key={`cell-${index}`}
+                                        fill={COLORS[index % COLORS.length]}
+                                    />
+                                ))}
+                    </Pie>
+                    <Tooltip />
+                    <Legend />
+                    
+                        
+                </PieChart>
+            </ResponsiveContainer>
+
         </Container>
 
     );
