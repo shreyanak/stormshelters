29d0f0cf39cf026e522e6fab36a0257ea7a3aa8e
jrsmith0 <johnrsmith20003@gmail.comqsfuckgit config --global user.email johnrsmith20003@gmail.com>
2023-11-29 18:23:03 -0600

    Provider visualization for Number of Neighborhoods by Pop (line chart)

--- frontend/src/components/provider-vis.js
+++ frontend/src/components/provider-vis.js
@@ -11,7 +11,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import '../css/Visualization.css';
 
-import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid, Legend } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid, Legend, Line, LineChart } from 'recharts';
 
 // https://backend.metropulse.link/centers?borough=bronx
 
@@ -33,7 +33,57 @@ function getCenters() {
     return values;
 }
 
+function getPops() {
+    var values = [
+        {
+            Range: "< 20,000",
+            Number: 0
+        },
+        {
+            Range: "20,000-30,000",
+            Number: 0
+        },
+        {
+            Range: "30,000-40,000",
+            Number: 0
+        },
+        {
+            Range: "40,000-50,000",
+            Number: 0
+        },
+        {
+            Range: "> 50,000",
+            Number: 0
+        }, 
+        
+    ];
+    var mainURL = `https://backend.metropulse.link/neighborhoods`;
+    var mainreq = new XMLHttpRequest();
+    mainreq.open('GET', mainURL, false); 
+    mainreq.send(null);
+    var json = JSON.parse(mainreq.responseText);
+    console.log(json);
+    for (var neighboorhood in json.data) {
+        var popToAdd = json.data[neighboorhood].population;
+        if (popToAdd < 20000) {
+            values[0].Number++;
+        } else if (popToAdd > 20000 && popToAdd < 30000) {
+            values[1].Number++;
+        } else if (popToAdd > 30000 && popToAdd < 40000) {
+            values[2].Number++;
+        } else if (popToAdd > 40000 && popToAdd < 50000) {
+            values[3].Number++;
+        } else if (popToAdd > 50000 ) {
+            values[4].Number++;
+        }
+    }
+    return values;
+}
+
+
 function ProviderVis() {
+    var popRanges = getPops();
+    console.log(popRanges[1].Number)
     var centersPerBorough = getCenters();
     // console.log("mydata: " + myvals);
 
@@ -54,7 +104,21 @@ function ProviderVis() {
                     <Bar dataKey="Centers" fill="#515487" width="20%"/>
                 </BarChart>
             </ResponsiveContainer>
+
+            <h2 className="text-center">Number of Neighborhoods By Population</h2>
+
+            <ResponsiveContainer width = "80%" height={450} className="center">
+                <LineChart width = {580} height={360} data={popRanges}>
+                    <CartesianGrid strokeDasharray="1 1" />
+                    <XAxis dataKey="Range"></XAxis>
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="Number" fill="#515487" width="20%"/>
+                </LineChart>
+            </ResponsiveContainer>
         </Container>
+
     );
 }
 
